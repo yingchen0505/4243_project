8351f2f00f149f6fcd36bb39085f0b0b22a65f3b
yingchen <lai.yingchen@u.nus.edu>
2019-10-10 15:54:31 +0800

    ransac

--- find_waldo2.py
+++ find_waldo2.py
@@ -39,11 +39,32 @@ matches = bf.knnMatch(des1,des2,k=2)
 good = []
 for m,n in matches:
     if m.distance < 0.75*n.distance:
-        good.append([m])
-# cv.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        good.append(m)
+        # good.append([m])
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+else:
+    print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+    matchesMask = None
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+# cv2.drawMatchesKnn expects list of lists as matches.
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+# img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 plt.imshow(img3)
-plt.savefig(test_image_ID + '_dsift_results.jpg', dpi=2000)
+plt.savefig(test_image_ID + '_sift_results_ransac.jpg', dpi=2000)
 
 # Initiate SIFT detector
 # sift = cv2.xfeatures2d.SIFT_create()
