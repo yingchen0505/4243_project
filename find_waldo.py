1bcc6d8fd11749b2f57d764ae87ea49f1a9308c8
yingchen <lai.yingchen@u.nus.edu>
2019-10-17 14:12:26 +0800

    added try catch to run all the eval images we can

--- find_waldo.py
+++ find_waldo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import sys, traceback
 
 MIN_MATCH_COUNT = 10
 
@@ -8,7 +9,7 @@ output_file = open("output/waldo.txt", "w+")
 
 # template_path = 'datasets/{}.jpg'
 
-img1 = cv2.imread('datasets/Templates/waldo.jpg', 0)  # queryImage
+img1 = cv2.imread('datasets/Templates/waldo.jpg', flags=cv2.IMREAD_GRAYSCALE)  # queryImage
 
 test_ids_file = open("datasets/ImageSets/val.txt", 'r')
 test_ids = test_ids_file.readlines()
@@ -16,61 +17,66 @@ test_ids = test_ids_file.readlines()
 for test_image_ID in test_ids:
     test_image_ID = test_image_ID.strip('\n')
     print('datasets/JPEGImages/' + test_image_ID + '.jpg')
-    img2 = cv2.imread('datasets/JPEGImages/' + test_image_ID + '.jpg', 0)  # trainImage
-    # plt.imshow(img2)
-    # plt.show()
-    # Initiate SIFT detector
-    sift = cv2.xfeatures2d.SIFT_create()
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+    try:
+        img2 = cv2.imread('datasets/JPEGImages/' + test_image_ID + '.jpg', flags=cv2.IMREAD_GRAYSCALE)  # trainImage
+        plt.imshow(img2)
+        plt.show()
+        # Initiate SIFT detector
+        sift = cv2.xfeatures2d.SIFT_create()
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
 
-    # BFMatcher with default params
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
-    # Apply ratio test
-    good = []
-    for m, n in matches:
-        if m.distance < 0.75 * n.distance:
-            good.append(m)
+        # BFMatcher with default params
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(des1, des2, k=2)
+        # Apply ratio test
+        good = []
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good.append(m)
 
-    if len(good) > MIN_MATCH_COUNT:
-        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-        matchesMask = mask.ravel().tolist()
-        h, w = img1.shape
-        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
-        dst = cv2.perspectiveTransform(pts, M)
-        img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
-        draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
-                           singlePointColor=None,
-                           matchesMask=matchesMask,  # draw only inliers
-                           flags=2)
+        if len(good) > MIN_MATCH_COUNT:
+            src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+            dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            matchesMask = mask.ravel().tolist()
+            h, w = img1.shape
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+            dst = cv2.perspectiveTransform(pts, M)
+            img2 = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+            draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                               singlePointColor=None,
+                               matchesMask=matchesMask,  # draw only inliers
+                               flags=2)
 
-        # cv2.drawMatchesKnn expects list of lists as matches.
-        img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+            # cv2.drawMatchesKnn expects list of lists as matches.
+            img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
 
-        h, w = img1.shape[:2]
-        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+            h, w = img1.shape[:2]
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
 
-        dst = cv2.perspectiveTransform(pts, M)
-        dst += (w, 0)  # adding offset
+            dst = cv2.perspectiveTransform(pts, M)
+            dst += (w, 0)  # adding offset
 
-        img3 = cv2.polylines(img3, [np.int32(dst)], True, (0, 0, 255), 3, cv2.LINE_AA)
+            img3 = cv2.polylines(img3, [np.int32(dst)], True, (0, 0, 255), 3, cv2.LINE_AA)
 
-        plt.imshow(img3)
-        plt.savefig('output/' + test_image_ID + '_sift_results_ransac_boundingbox.jpg', dpi=2000)
+            plt.imshow(img3)
+            plt.savefig('output/' + test_image_ID + '_sift_results_ransac_boundingbox.jpg', dpi=2000)
 
-        # Write output test file
-        xmin = np.min(dst[:, :, 0])
-        ymin = np.min(dst[:, :, 1])
-        xmax = np.max(dst[:, :, 0])
-        ymax = np.max(dst[:, :, 1])
-        output_file.write(test_image_ID + " 1.000 %.1f" % xmin + " %.1f" % ymin + " %.1f" % xmax + " %.1f" % ymax + " \n")
+            # Write output test file
+            xmin = np.min(dst[:, :, 0])
+            ymin = np.min(dst[:, :, 1])
+            xmax = np.max(dst[:, :, 0])
+            ymax = np.max(dst[:, :, 1])
+            output_file.write(test_image_ID + " 1.000 %.1f" % xmin + " %.1f" % ymin + " %.1f" % xmax + " %.1f" % ymax + " \n")
 
-    else:
-        print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
-        matchesMask = None
+        else:
+            print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
+            matchesMask = None
+
+    except:
+        print(test_image_ID + ' failed')
+        traceback.print_exc(file=sys.stdout)
 
 output_file.close()
