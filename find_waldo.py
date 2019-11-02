corrected bug in scaling of bboxes and improved efficiency

find_waldo.py
@@ -29,31 +29,41 @@ image_ids_file = open("datasets/ImageSets/val.txt", 'r')
 # image_ids_file = open("datasets/ImageSets/train.txt", 'r')
 image_ids = image_ids_file.readlines()
 
+template_sifts = []
+
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
+
+# Build sift for all the templates
+for index, template in enumerate(templates):
+    img1 = cv2.imread(template, flags=cv2.IMREAD_GRAYSCALE)  # queryImage
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    template_sifts.append({'kp1': kp1, 'des1': des1, 'img1': img1})
+
 # Run on each image
 for image_id in image_ids:
     image_id = image_id.strip('\n')
     print('datasets/JPEGImages/' + image_id + '.jpg')
 
-    for index, template in enumerate(templates):
-        img1 = cv2.imread(template, flags=cv2.IMREAD_GRAYSCALE)  # queryImage
+    try:
+        img2 = cv2.imread('datasets/JPEGImages/' + image_id + '.jpg', flags=cv2.IMREAD_GRAYSCALE)  # trainImage
+        scaling_factor = np.ceil(max(img2.shape) / 1600)
+        print(img2.shape)
+        print(scaling_factor)
+        new_size = np.array(np.rint(img2.shape / scaling_factor), dtype=int)
+        new_size = np.array((new_size[1], new_size[0]))
+        new_size = tuple(new_size)
+        print(new_size)
+        img2_resized = cv2.resize(img2, new_size)
+        plt.imshow(img2_resized)
+        plt.show()
+        kp2, des2 = sift.detectAndCompute(img2_resized, None)
 
-        try:
-            img2 = cv2.imread('datasets/JPEGImages/' + image_id + '.jpg', flags=cv2.IMREAD_GRAYSCALE)  # trainImage
-            scaling_factor = np.ceil(max(img2.shape) / 1600)
-            print(img2.shape)
-            print(scaling_factor)
-            new_size = np.array(np.rint(img2.shape/scaling_factor), dtype=int)
-            new_size = np.array((new_size[1], new_size[0]))
-            new_size = tuple(new_size)
-            print(new_size)
-            img2_resized = cv2.resize(img2, new_size)
-            plt.imshow(img2_resized)
-            plt.show()
-            # Initiate SIFT detector
-            sift = cv2.xfeatures2d.SIFT_create()
-            # find the keypoints and descriptors with SIFT
-            kp1, des1 = sift.detectAndCompute(img1, None)
-            kp2, des2 = sift.detectAndCompute(img2_resized, None)
+        for index, template_sift in enumerate(template_sifts):
+            kp1 = template_sift['kp1']
+            des1 = template_sift['des1']
+            img1 = template_sift['img1']
 
             # BFMatcher with default params
             bf = cv2.BFMatcher()
@@ -93,7 +103,7 @@ for image_id in image_ids:
                 plt.savefig('output/' + image_id + '_' + template_names[index] + '.jpg', dpi=2000)
 
                 # Scale back
-                dst /= scaling_factor
+                dst *= scaling_factor
 
                 # Write output test file
                 xmin = np.min(dst[:, :, 0])
@@ -106,9 +116,8 @@ for image_id in image_ids:
                 print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
                 matchesMask = None
 
-        except:
-            print(image_id + ' failed')
-            traceback.print_exc(file=sys.stdout)
-            break
+    except:
+        print(image_id + ' failed')
+        traceback.print_exc(file=sys.stdout)
 
 output_file.close()